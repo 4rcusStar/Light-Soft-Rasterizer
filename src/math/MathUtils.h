3db15773e9460bb171f3e418c4a50a19700f4d23
4cr <4rcusSte11a@gmail.com>
2026-04-03 02:38:54 +0800

    -新增摄像机类 -完成vertexShader的所有变换函数

--- src/math/MathUtils.h
+++ src/math/MathUtils.h
@@ -9,8 +9,6 @@
 
 class MathUtils
 {
-    //TODO:创建重心插值函数
-    ///
     /// @param v 要插值的坐标
     /// @param a A点
     /// @param b B点
@@ -27,6 +25,28 @@ public:
         beta = (-(v.x-c.x)*(a.y-c.y)+(v.y-c.y)*(a.x-c.x))/(-(b.x-c.x)*(a.y-c.y)+(b.y-c.y)*(a.x-c.x));
         gamma = 1-alpha-beta;
         return {alpha,beta,gamma};
+    }
+
+    /// 将值从源区间线性映射到右区间
+    /// @tparam T
+    /// @param value 待映射的输入
+    /// @param originLeft 源区间左边界
+    /// @param originRight 源区间右边界
+    /// @param desLeft 目标区间左边界
+    /// @param desRight 目标区间右边界
+    /// @return 映射后的值
+    template<typename T>
+    static T remap(T value,T originLeft,T originRight,T desLeft,T desRight)
+    {
+        const T originRange = originRight - originLeft;
+
+        if (std::abs(originRange) < std::numeric_limits<T>::epsilon())
+        {
+            return desLeft;
+        }
+
+        const T normalized = (value - originLeft) / originRange;
+        return desLeft + normalized * (desRight - desLeft);
     }
 };
 
